3da9298fbf42f05b04f51b3ca91d6efc01bfa881
deTheux_Boris <boris.detheux@student.vinci.be>
2020-12-10 23:16:19 +0100

    Supp de trucs inutiles + quelques comm

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,28 +1,17 @@
 import { RedirectUrl } from "./Router.js";
 import { getUserSessionData } from "../utils/session.js";
 import { API_URL } from "../utils/server.js";
-//import { setLayout } from "../utils/render.js";
+
 let user;
 let profilePage;
 
 
-/*else{
-    profilePage = `<div class="container-fluid panneau">
-    <ul class="cadre-panneau-profile">
-        <li class="username-profile"><b>Username:</b></li>
-        <li class="last-connected"><b>Last connected at: </b></li>
-        <li>
-            <a href="/logout" data-uri="/logout" class="btn btn-danger logout-btn" role="button">Log-out</a>
-        </li>
-    </ul>
-    </div>` 
-};*/
-
-
 const Profile = () => {
-    //setLayout("About Us");
     user  = getUserSessionData(); 
-    console.log(user);
+
+   /*Si l'utilisateur n'est pas connecté alors il est redirigé vers la page de login 
+   *sinon on affiche son username et bientot sa dernière connexion
+   */
     if (!user) {
         RedirectUrl("/loginRegister");
     }else{
