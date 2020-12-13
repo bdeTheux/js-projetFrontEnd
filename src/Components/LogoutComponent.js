a88364004a9f23c4e43743585bec6ab44d114520
deTheux_Boris <boris.detheux@student.vinci.be>
2020-12-13 17:15:25 +0100

    Ajout router logout

--- src/Components/LogoutComponent.js
+++ src/Components/LogoutComponent.js
@@ -1,12 +1,14 @@
 import LoginRegisterPage from "./LoginRegisterPage.js";
-import { RedirectUrl } from "./Router.js";
+import { RedirectUrl, Router } from "./Router.js";
 import Navbar from "./Navbar.js";
 import {removeSessionData} from "../utils/session.js";
 
 const Logout = () => {
+  console.log("hello")
   removeSessionData();
   Navbar();
   RedirectUrl("/");
+  Router();
 };
 
 export default Logout;
